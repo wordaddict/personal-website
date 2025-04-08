cde79020161a5ee728db396bab28b37b80c7d6b5
Micheal Adeyinka <michealadeyinka@Micheals-MacBook-Pro.local>
2025-04-08 09:54:30 -0400

    add vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        preview: {
-          allowedHosts: ['personalwe-059c4b0c0362.herokuapp.com']
-        }
+      preview: {
+        host: '0.0.0.0',
+        port: Number(process.env.PORT || 4321),
+        allowedHosts: ['personalwe-059c4b0c0362.herokuapp.com']
       }
-});
+    }
+  });
